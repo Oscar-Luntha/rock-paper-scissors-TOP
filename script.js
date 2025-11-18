d3cf9a137f49d2287043c27de1fb8012ea15ca26
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-18 09:43:32 +0200

    Map the random numbers generated to either rock, paper or scissors and return the value

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random() * 3) + 1 
-    console.log(computerChoice)
+    if(computerChoice === 1){
+        computerChoice = "ROCK"
+    }else if(computerChoice === 2) {
+        computerChoice = "PAPER"
+    }
+    else{
+        computerChoice = "SCISSORS"
+    }
+    return computerChoice
 }
-getComputerChoice()
+console.log(getComputerChoice())
