124103e5752536019cd915b14b7cdfdd0f16dd4b
Oscar Luntha <lunthaoscar@gmail.com>
2025-11-19 09:50:37 +0200

    Create a function that plays five rounds of the game and keeps track of the human and computer score

--- script.js
+++ script.js
@@ -14,30 +14,60 @@ function getHumanChoice(){
     let humanChoice = prompt("Rock , Paper , Scissors").toUpperCase()
     return humanChoice
 }
-let humanScore = 0;
-let computerScore = 0;
+
 function playRound(humanChoice, computerChoice){
     if (humanChoice == computerChoice){
         console.log("same selection! no score to both players")
+        return 2
     }
     if (humanChoice == "ROCK"){
         if (computerChoice == "PAPER"){
             console.log ( "you lose, paper beats rock")
+            return 0
         }
-        else console.log("you win")
+        else {
+            console.log("you win")
+            return 1
+        }
     } else if (humanChoice == "PAPER"){
         if (computerChoice == "ROCK"){
             console.log ("you win")
+            return 1
         }
-        else console.log("You lose, scissors beat paper")
+        else {
+            console.log("You lose, scissors beat paper")
+            return 0
+        }
     } else{
         if (computerChoice == "ROCK"){
             console.log("You lose!. scissors beat rock")
-        }else console.log("You win")
+            return 0
+        }else{
+            console.log("You win")
+            return 1
+        } 
 
     }
 }
-const computerSelection =getComputerChoice()
-const humanSelection = getHumanChoice()
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let i = 0;
+    while(i <= 4){
+        const computerSelection =getComputerChoice()
+        const humanSelection = getHumanChoice()
+        score = playRound(humanSelection, computerSelection)
+        if (score == 1 ){
+            humanScore += 1
+        } else if (score == 0 )computerScore +=1
+        else {
+            humanScore += 0
+            computerScore += 0
+        }
+        i+=1;
+    }
+    alert(`Your score ${humanScore} : ${computerScore} Computer score`)
+}
+playGame()
 
-playRound (humanSelection, computerSelection)
+
